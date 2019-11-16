1b587a411931f39789fa9a86b8c1d7798fa6ef1f
essential-robot-4364 <brandon.james414@gmail.com>
2019-11-16 17:41:40 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,19 @@
+class Song 
+   attr_accessor :name, :artist, :genre
+   
+   @@count = 0 
+   @@genres = []
+   @@artists = []
+  
+  def initialize(name, artist, genre)
+    @@count += 1 
+    @name = name
+    @genre = genre 
+    @@genres << genre
+    @artist = artist
+    @@artists << artist
+  end
+  
+ 
+ 
+end
