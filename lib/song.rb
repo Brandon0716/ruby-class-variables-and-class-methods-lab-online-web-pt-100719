b915849a4f4090bbb0815b1d30cdf3568d90313f
essential-robot-4364 <brandon.james414@gmail.com>
2019-11-16 18:10:15 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -28,8 +28,16 @@ class Song
  
  def self.genre_count
    @@genre_count
-   
- end
+  genre_count = {}
+    @@genres.each do |genre| 
+      if genre_count[genre]
+      genre_count[genre] += 1 
+      else
+      genre_count[genre] = 1
+      end
+    end
+    genre_count
+  end
 
  
 end
